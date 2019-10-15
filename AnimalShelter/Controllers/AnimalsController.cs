5fa65fe97233e0e6e1636f7212a57ba7393f77db
Student <example@example.com>
2019-10-15 15:57:09 -0700

    add viewbag

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -3,6 +3,7 @@ using AnimalShelter.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 
 namespace AnimalShelter.Controllers
@@ -13,17 +14,23 @@ namespace AnimalShelter.Controllers
 
         public AnimalsController(AnimalShelterContext db)
         {
-        _db = db;
+            _db = db;
         }
 
         public ActionResult Index()
         {
-        List<Animal> model = _db.Animals.ToList();
-        return View(model);
+            List<Animal> model = _db.Animals.Include(animals => animals.Species).ToList();
+            return View(model);
         }
+
+        // public ActionResult Create()
+        // {
+        //     return View();
+        // }
 
         public ActionResult Create()
         {
+            ViewBag.SpeciesID = new SelectList(_db.Species, "ID", "Name");
             return View();
         }
 
@@ -35,6 +42,7 @@ namespace AnimalShelter.Controllers
             return RedirectToAction("Index");
         }
 
+
         public ActionResult Details(int id)
         {
             Animal thisAnimal= _db.Animals.FirstOrDefault(animals => animals.ID == id);
@@ -43,6 +51,7 @@ namespace AnimalShelter.Controllers
         public ActionResult Edit(int id)
         {
             var thisAnimal = _db.Animals.FirstOrDefault(animals => animals.ID == id);
+            ViewBag.SpeciesID = new SelectList(_db.Species, "ID", "Name");
             return View(thisAnimal);
         }
 
--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,10 +1,12 @@
 using System;
-  
+
 namespace AnimalShelter.Models
 {
   public class Animal
   {
     public int ID { get; set; }
+    public int SpeciesID { get; set; }
+    public virtual Species Species { get; set; }
     public string Type { get; set; }
     public string Name { get; set; }
     public string Gender { get; set; }
